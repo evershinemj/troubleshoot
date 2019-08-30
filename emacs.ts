6842d99e114f49c6c73080fceec62d9292d5b9ed
wangxueming@windows <459226479@qq.com>
2019-08-31 06:37:50 +0800

    On branch master 	modified:   emacs.ts         troubleshooting the problem 'ctrl-<spc> does not work in emacs'

--- emacs.ts
+++ emacs.ts
@@ -1,3 +1,9 @@
 ~/.emacs doesn't work for emacs on windows
 solution:
     init file for emacs on windows is ~/AppData/Roaming/.emacs
+
+ctrl-<spc> doesn't work in emacs
+solution:
+    think about the cause. it's due to the fact that ctrl-<spc> is already sent to system before otherwise being sent to emacs.
+    in other words, it's used by another application. in this case, it's the sougou input method.
+    hence, switch input method to English and the problem will be solved.
